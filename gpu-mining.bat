79a332eb8c0f6cf63fb5722d4680f39651c55b3a
farsider350 <31876349+farsider350@users.noreply.github.com>
2018-12-22 15:42:45 +1000

    Initial

---
+++ gpu-mining.bat
@@ -0,0 +1,4 @@
+:mine
+ccminer.exe  -a scrypt -o http://127.0.0.1:1986 -u hello -p hellou -i 21 --no-longpoll --no-getwork --no-stratum --coinbase-addr=AQgNQQSboKLMM25EzUdigGgJ8RZEEL2k8S
+ping -n 30 127.0.0.1 
+goto :mine
